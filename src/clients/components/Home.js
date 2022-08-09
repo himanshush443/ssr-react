8bdc3db3560729ff4961eefecf6037280711022e
Himanshu Khare <himanshush443@gmail.com>
2022-08-09 23:56:37 +0530

    server side webpack set

---
+++ src/clients/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <div>Home Comp Rebuild</div>
+      <button onClick={() => console.log('Hi There')}>Clcik me</button>
+    </div>
+  );
+}
